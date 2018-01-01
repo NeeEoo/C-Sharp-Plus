e4481eb775bc8f5cb8c314193b7b2964a8cb98cf
NeeEoo <Neoscarsen@gmail.com>
2018-01-01 20:45:02 +0100

    Console::Clear()

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include "windows.h"
 
 using std::string;
 using namespace std;
@@ -21,10 +22,39 @@ namespace CSP {
 		static void Write(string value) {
 			cout << value;
 		}
-		static std::string ReadLine() {
+		static string ReadLine() {
 			string s;
 			getline(cin, s);
 			return s;
+		}
+		static void Clear()
+		{
+			HANDLE                     hStdOut;
+			CONSOLE_SCREEN_BUFFER_INFO csbi;
+			DWORD                      count;
+			DWORD                      cellCount;
+			COORD                      homeCoords = { 0, 0 };
+
+			hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+			if (hStdOut == INVALID_HANDLE_VALUE) {
+				return;
+			}
+			
+			if (!(GetConsoleScreenBufferInfo(hStdOut, &csbi))) {
+				return;
+			}
+
+			cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+			if (!(FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount, homeCoords, &count))) {
+				return;
+			}
+
+			if (!(FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count))) {
+				return;
+			}
+			SetConsoleCursorPosition(hStdOut, homeCoords);
+
 		}
 	}
 }
