b9c52e46a439fb2eefff81648740849ac234b9b1
NeeEoo <Neoscarsen@gmail.com>
2018-01-02 20:23:22 +0100

    Environment::MachineName

--- Environment/Environment.cpp
+++ Environment/Environment.cpp
@@ -16,7 +16,16 @@ namespace CSP {
 			wstring wStr = name;
 			return string(wStr.begin(), wStr.end());
 		}
+		const string PrivateMachineName() {
+			TCHAR name[UNLEN + 1];
+			DWORD size = UNLEN + 1;
+
+			GetComputerName((TCHAR*)name, &size);
+			wstring wStr = name;
+			return string(wStr.begin(), wStr.end());
+		}
 		static const string UserName = PrivateUserName();
+		static const string MachineName = PrivateMachineName();
 		//static const string UserNameS = GetUserName((TCHAR*)name[257], (LPDWORD)257);
 	}
 }
