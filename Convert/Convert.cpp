312b295831f506b7dffbfc32a16524f59847eedd
NeeEoo <Neoscarsen@gmail.com>
2018-01-12 20:57:29 +0100

    Blocks to Curly Brackets

--- Convert/Convert.cpp
+++ Convert/Convert.cpp
@@ -116,30 +116,36 @@ namespace CSP {
 			while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
 				char_array_4[i++] = encoded_string[in_]; in_++;
 				if (i == 4) {
-					for (i = 0; i <4; i++)
+					for (i = 0; i < 4; i++) {
 						char_array_4[i] = base64_chars.find(char_array_4[i]);
+					}
 
 					char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
 					char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
 					char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-					for (i = 0; (i < 3); i++)
+					for (i = 0; (i < 3); i++) {
 						ret += char_array_3[i];
+					}
 					i = 0;
 				}
 			}
 			if (i) {
-				for (j = i; j <4; j++)
+				for (j = i; j < 4; j++) {
 					char_array_4[j] = 0;
+				}
 
-				for (j = 0; j <4; j++)
+				for (j = 0; j < 4; j++) {
 					char_array_4[j] = base64_chars.find(char_array_4[j]);
+				}
 
 				char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
 				char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
 				char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-				for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+				for (j = 0; (j < i - 1); j++) {
+					ret += char_array_3[j]; 
+				}
 			}
 			return ret;
 		}
