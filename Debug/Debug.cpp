62728726c2c7a4dddc50e2a5fbc4a27145b30b66
NeeEoo <Neoscarsen@gmail.com>
2018-02-12 18:48:46 +0100

    Added wstring support for "Debug::Write()" and "Debug::WriteLine()"

--- Debug/Debug.cpp
+++ Debug/Debug.cpp
@@ -7,20 +7,36 @@ namespace CSP {
 	//using System.Diagnostics;
 	class Debug {
 	public:
+		static void Write(string value) {
+			OutputDebugStringA(value.c_str());
+		}
+		static void Write(const char *value) {
+			OutputDebugStringA(value);
+		}
 		static void Write(wstring value) {
 			OutputDebugStringW(value.c_str());
 		}
-		static void Write(string value) {
+		static void Write(const wchar_t *value) {
+			OutputDebugStringW(value);
+		}
+		static void WriteLine(string value) {
+			value = value + "\n";
 			OutputDebugStringA(value.c_str());
+		}
+		static void WriteLine(const char *value) {
+			const char* s = "\n";
+			value = value + *s;
+			OutputDebugStringA(value);
 		}
 		static void WriteLine(wstring value) {
 			wstring s = L"\n";
 			value = value.c_str() + s;
 			OutputDebugStringW(value.c_str());
 		}
-		static void WriteLine(string value) {
-			value = value + "\n";
-			OutputDebugStringA(value.c_str());
+		static void WriteLine(const wchar_t *value) {
+			const wchar_t* s = L"\n";
+			value = value + *s;
+			OutputDebugStringW(value);
 		}
 	};
 }
