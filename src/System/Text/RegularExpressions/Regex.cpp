22ca0d0bccd592d16ce0fe046e97bd9280965c46
NeeEoo <Neoscarsen@gmail.com>
2018-03-04 19:06:44 +0100

    Added 1 more overload to Regex::Replace Added 1 more overload to Regex::IsMatch

--- src/System/Text/RegularExpressions/Regex.cpp
+++ src/System/Text/RegularExpressions/Regex.cpp
@@ -7,11 +7,22 @@ namespace CSP {
 			namespace RegularExpressions {
 				class Regex {
 				public:
+					static string Replace(string input, regex pattern, string replacement) {
+						return regex_replace(input, pattern, replacement);
+					}
 					static string Replace(string input, string pattern, string replacement) {
 						return regex_replace(input, regex(pattern), replacement);
 					}
 					static bool IsMatch(string input, string pattern) {
 						if (regex_search(input, regex(pattern))) {
+							return true;
+						}
+						else {
+							return false;
+						}
+					}
+					static bool IsMatch(string input, regex pattern) {
+						if (regex_search(input, pattern)) {
 							return true;
 						}
 						else {
